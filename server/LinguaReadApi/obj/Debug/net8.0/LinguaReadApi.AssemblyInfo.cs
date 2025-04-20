af710bb4f0103860e506af49416c3dd7d7fa6190
semblable <kamil.u.1998@gmail.com>
2025-04-20 11:02:38 +0200

    restore from backup fixed

--- server/LinguaReadApi/obj/Debug/net8.0/LinguaReadApi.AssemblyInfo.cs
+++ server/LinguaReadApi/obj/Debug/net8.0/LinguaReadApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinguaReadApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2cd87dfc0a00415118bb6f33d8d4bbdd22ce1fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4141342bb69dfb4a20a8b71256e9f67838adf9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinguaReadApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinguaReadApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
