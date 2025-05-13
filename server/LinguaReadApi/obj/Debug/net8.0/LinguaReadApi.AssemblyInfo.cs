6933708e11010c615a40186a2438efafba953fc2
semblable <kamil.u.1998@gmail.com>
2025-05-13 13:03:53 +0200

    http

--- server/LinguaReadApi/obj/Debug/net8.0/LinguaReadApi.AssemblyInfo.cs
+++ server/LinguaReadApi/obj/Debug/net8.0/LinguaReadApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinguaReadApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3cb0859a18e1e372622de116c669f7f417f4fe9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6952d8f4400897ada1c67f34393383bc080779d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinguaReadApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinguaReadApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
